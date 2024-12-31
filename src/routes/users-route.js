5bc4be9c3da6db12dd5d3abbe15110420168e4f7
evanalifian <evanalifian@gmal.com>
2024-12-31 18:42:36 +0700

    change route name for users

--- src/routes/users-route.js
+++ src/routes/users-route.js
@@ -1,13 +1,13 @@
 import express from "express"
 
-export const route = express.Router()
+export const usersRoute = express.Router()
 
 let users
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(values => values.json())
   .then(values => users = values)
 
-route.get("/", (req, res) => {
+usersRoute.get("/", (req, res) => {
   res.render("users/index.html", {
     title: "Users",
     users
